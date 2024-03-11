cd4d69d4dc21fd274fa1163c40f34e349c8eba28
joegasewicz <joegasewicz@gmail.com>
2024-03-11 20:28:27 +0000

    #3 setup tests

---
+++ tests/arobase.spec.ts
@@ -0,0 +1,12 @@
+import {Arobase} from "../src";
+
+describe("observer", () => {
+
+    describe("#Arobase()", () => {
+        it("should create an instance of modelSubject", () => {
+            // const model = {name: "Joe"};
+            // const arobase = new Arobase("testApp", model);
+            // expect(arobase).toBeInstanceOf(Arobase);
+        });
+    });
+});
